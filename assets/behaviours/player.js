23cdb0ccc30bbf003ed4950bdadf543bdc00b636
Mefteg <gimenez.tom@gmail.com>
2014-12-06 19:35:45 -0500

    Add correct scale when player is jumping.

--- assets/behaviours/player.js
+++ assets/behaviours/player.js
@@ -14,7 +14,8 @@ Player.prototype.create = function( _data ){
     this.scaleMin = 0.5;
     this.scaleMax = 1;
 
-    this.timeInAirMax = 300; // in ms
+    this.timeInAirMax = 400; // in ms
+    this.timeInGroundMin = 50; // in ms
 
     this.reset();
 };
@@ -34,6 +35,7 @@ Player.prototype.reset = function(){
 
     this.onGround = true;
     this.timeInAir = 0;
+    this.timeInGround = 0;
 
     this.active = true;
 };
@@ -45,57 +47,83 @@ Player.prototype.update = function() {
     if (this.active == true) {
         var game = this.go.game;
 
-        this.direction.setTo(0, 0);
+        this.updatePosition(dt);
 
-        if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-            this.direction.x -= 1;
-        }
-
-        if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-            this.direction.x += 1;
-        }
-
-        if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-            this.direction.y -= 1;
-        }
-
-        if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-            this.direction.y += 1;
-        }
-
-        this.direction.normalize();
-
-        var halfWidth = this.go.game.camera.width * 0.5;
-        this.entity.x += this.direction.x * this.speed * dt;
-        if (this.entity.x - this.entity.width * 0.5 < -halfWidth)
-            this.entity.x = -halfWidth + this.entity.width * 0.5;
-        else
-            if (this.entity.x + this.entity.width * 0.5 > halfWidth)
-                this.entity.x = halfWidth - this.entity.width * 0.5;
-
-        var halfHeight = this.go.game.camera.height * 0.5;
-        this.entity.y += this.direction.y * this.speed * dt;
-        if (this.entity.y - this.entity.height * 0.5 < -halfHeight + 62) // 62 for the header
-            this.entity.y = -halfHeight + 62 + this.entity.height * 0.5; // 62 for the header
-        else
-            if (this.entity.y + this.entity.height * 0.5 > halfHeight)
-                this.entity.y = halfHeight - this.entity.height * 0.5;
-
+        // if player is on ground
         if (this.onGround == true) {
-            if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
-                this.entity.scale.setTo(this.scaleMax, this.scaleMax);
-                this.timeInAir = 0;
-                this.onGround = false;
+            // if he is on ground long enough
+            if (this.timeInGround > this.timeInGroundMin) {
+                // if the player want to jump
+                if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+                    this.entity.scale.setTo(this.scaleMin, this.scaleMin);
+                    this.timeInAir = 0;
+                    this.onGround = false;
+                }
+            } else {
+                this.timeInGround += dt;
+                if (this.timeInGround > 10000) this.timeInGround = 10000; // prevent too big timer
             }
         } else {
+            // if the player has to land
             if (this.timeInAir >= this.timeInAirMax) {
                 this.entity.scale.setTo(this.scaleMin, this.scaleMin);
+                this.timeInGround = 0;
                 this.onGround = true;
             } else {
                 this.timeInAir += dt;
+
+                // update scaling
+                var ratio = this.timeInAir / this.timeInAirMax;
+                if (ratio > 1) ratio = 1;
+                var doubleDeltaScale = (this.scaleMax - this.scaleMin) * 2;
+                var newScale = this.scaleMin + doubleDeltaScale * Math.sin(ratio);
+                if (ratio > 0.5) {
+                    newScale = this.scaleMin + doubleDeltaScale * (1 - Math.sin(ratio));
+                }
+                this.entity.scale.setTo(newScale, newScale);
             }
         }
     }
+};
+
+Player.prototype.updatePosition = function(_dt) {
+    var game = this.go.game;
+
+    this.direction.setTo(0, 0);
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+        this.direction.x -= 1;
+    }
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+        this.direction.x += 1;
+    }
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+        this.direction.y -= 1;
+    }
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+        this.direction.y += 1;
+    }
+
+    this.direction.normalize();
+
+    var halfWidth = this.go.game.camera.width * 0.5;
+    this.entity.x += this.direction.x * this.speed * _dt;
+    if (this.entity.x - this.entity.width * 0.5 < -halfWidth)
+        this.entity.x = -halfWidth + this.entity.width * 0.5;
+    else
+        if (this.entity.x + this.entity.width * 0.5 > halfWidth)
+            this.entity.x = halfWidth - this.entity.width * 0.5;
+
+    var halfHeight = this.go.game.camera.height * 0.5;
+    this.entity.y += this.direction.y * this.speed * _dt;
+    if (this.entity.y - this.entity.height * 0.5 < -halfHeight + 62) // 62 for the header
+        this.entity.y = -halfHeight + 62 + this.entity.height * 0.5; // 62 for the header
+    else
+        if (this.entity.y + this.entity.height * 0.5 > halfHeight)
+            this.entity.y = halfHeight - this.entity.height * 0.5;
 };
 
 Player.prototype.onGameOver = function() {
